f41eeb56fa1e5ea1d602295feeb1e0a1c4f16235
bright <1521488775@qq.com>
2019-09-06 08:00:36 +0800

    添加markdown编译和预览

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
-    
+    <mark-down/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
+import MarkDown from '../components/Markdown.vue';
 @Component({
   components: {
+    MarkDown
   },
 })
 export default class Home extends Vue {}
